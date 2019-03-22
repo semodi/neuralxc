189dfe2e894f5a2cc0625a78e5f48dfee048d7ed
Sebastian Dick <sebastian.dick@stonybrook.edu>
2019-03-22 10:04:20 -0400

    fixed reference file location for grouped_transformer test

--- neuralxc/tests/test_neuralxc.py
+++ neuralxc/tests/test_neuralxc.py
@@ -265,8 +265,10 @@ def test_formatter():
 
 @pytest.mark.fast
 @pytest.mark.parametrize(['transformer','filepath'],
-                         [[xc.ml.transformer.GroupedPCA(n_components=1), 'pca1.pckl'],
-                          [xc.ml.transformer.GroupedVarianceThreshold(0.005),'var09.pckl']])
+                         [[xc.ml.transformer.GroupedPCA(n_components=1),
+                          os.path.join(test_dir, 'pca1.pckl')],
+                          [xc.ml.transformer.GroupedVarianceThreshold(0.005),
+                          os.path.join(test_dir, 'var09.pckl')]])
 def test_grouped_transformers(transformer, filepath):
     with open(os.path.join(test_dir, 'h2o_rot2.pckl'),'rb') as file:
         C = pickle.load(file)
