steps towards fixing basis derivatives

neuralxc/projector/projector.py
@@ -154,7 +154,8 @@ class DensityProjector(BaseProjector):
             if calc_forces:
                 if not isinstance(rho, np.ndarray):
                     raise ValueError('Must provide rho as np.ndarray')
-                force_corrections[i] = self.delbasis(rho, coeffs, box, basis['n'], basis['l'],
+                force_corrections[i] = self.get_force_correction(rho, coeffs,
+                    box, basis['n'], basis['l'],
                     basis['r_o'], self.W[spec])
 
         if calc_forces:
@@ -185,7 +186,8 @@ class DensityProjector(BaseProjector):
         """
         return sph_harm(m,l,phi,theta)
 
-    def delbasis(self, rho, coeffs, box, n_rad, n_l, r_o, W = None):
+
+    def get_force_correction(self, rho, coeffs, box, n_rad, n_l, r_o, W = None):
         """ Calculate the contribution to the forces that arises from the
         dependence of the (nxc-)basis set on the atomic positions
 

neuralxc/tests/test_basis_grad.py
@@ -99,32 +99,100 @@ def test_dspher():
         assert np.allclose(ang_scipy, ang_soler)
 
     incr = 0.0000001
-    inc_idx = 0
-    for c in coords:
+    for inc_idx in range(3):
+        for c in coords:
+            dr = np.zeros(3)
+            dr[inc_idx] += incr
+            rp, thetap, phip = to_spher(c + dr)
+            rm, thetam, phim = to_spher(c - dr)
+            angp = np.array([rp**l*projector.angulars(l,m,thetap, phip) \
+                         for l in range(lmax + 1) \
+                         for m in range(-l,l+1)])
+
+            angm = np.array([rm**l*projector.angulars(l,m,thetam, phim) \
+                         for l in range(lmax + 1) \
+                         for m in range(-l,l+1)])
+
+            ang_solerp = rlylm(lmax, c + dr)
+            ang_solerm = rlylm(lmax, c - dr)
+            dang_fd_soler = (ang_solerp - ang_solerm)/(2*incr)
+            dang_fd = (angp-angm)/(2*incr)
+
+            dang_exact = grlylm(lmax, c)[inc_idx]
+            dang_exact = M.dot(dang_exact)
+            dang_fd_soler = M.dot(dang_fd_soler)
+            assert np.allclose(dang_fd, dang_exact, atol=incr)
+            assert np.allclose(dang_fd_soler, dang_exact, atol=incr)
+
+    incr = 0.00001
+    for inc_idx in range(3):
         dr = np.zeros(3)
         dr[inc_idx] += incr
-        rp, thetap, phip = to_spher(c + dr)
-        rm, thetam, phim = to_spher(c - dr)
-        angp = np.array([rp**l*projector.angulars(l,m,thetap, phip) \
-                     for l in range(lmax + 1) \
-                     for m in range(-l,l+1)])
+        mesh = np.meshgrid(*[np.linspace(0,2,6)]*3)
+        rp, thetap, phip = to_spher([r + dr_ for r,dr_ in zip(mesh,dr)])
+        rm, thetam, phim = to_spher([r - dr_ for r,dr_ in zip(mesh,dr)])
 
-        angm = np.array([rm**l*projector.angulars(l,m,thetam, phim) \
+        angp = [rp**l*projector.angulars(l,m,thetap, phip) \
                      for l in range(lmax + 1) \
-                     for m in range(-l,l+1)])
+                     for m in range(-l,l+1)]
 
-        ang_solerp = rlylm(lmax, c + dr)
-        ang_solerm = rlylm(lmax, c - dr)
-        dang_fd_soler = (ang_solerp - ang_solerm)/(2*incr)
+        angm = [rm**l*projector.angulars(l,m,thetam, phim) \
+                     for l in range(lmax + 1) \
+                     for m in range(-l,l+1)]
+
+        dang_fd = [(p-m)/(2*incr) for p,m in zip(angp,angm)]
+        n_l = lmax
+        M = xc.projector.M_make_complex(n_l)
+        X, Y, Z = mesh
+        dangs = []
+        for il in range(n_l**2):
+            dangs.append(np.zeros([len(X.flatten()),3]))
+
+        for ir, r in enumerate(zip(X.flatten(),Y.flatten(),Z.flatten())):
+            print(r)
+            vecspher = xc.projector.spher_grad.grlylm(n_l - 1, r) # shape: (3, n_l*n_l)
+            for il, vs in enumerate(vecspher.T):
+                dangs[il][ir] = vs
+
+        dangs = np.einsum('ij,jkl -> ikl', M, np.array(dangs))
+        dangs = dangs.reshape(len(dangs),*X.shape,3)[:,:,:,:,inc_idx]
+        for idx, (exact, fd) in enumerate(zip(dangs, dang_fd)):
+            assert np.allclose(exact, fd, atol = incr)
+
+    incr = 0.00001
+    l = 6
+    m = -3
+    for inc_idx in range(3):
+        pick = sum([1 for l_ in range(l) for m_ in range(-l_,l_+1)])
+        pick += (l+m)
+        dr = np.zeros(3)
+        dr[inc_idx] += incr
+        mesh = np.meshgrid(*[np.linspace(0,2,6)]*3)
+        rp, thetap, phip = to_spher([r + dr_ for r,dr_ in zip(mesh,dr)])
+        rm, thetam, phim = to_spher([r - dr_ for r,dr_ in zip(mesh,dr)])
+
+        angp = rp**l*projector.angulars(l,m,thetap, phip)
+
+        angm = rm**l*projector.angulars(l,m,thetam, phim)
+
         dang_fd = (angp-angm)/(2*incr)
+        n_l = lmax
+        M = xc.projector.M_make_complex(n_l)
+        X, Y, Z = mesh
+        dangs = []
+        for il in range(n_l**2):
+            dangs.append(np.zeros([len(X.flatten()),3]))
 
-        dang_exact = grlylm(lmax, c)[inc_idx]
-        dang_exact = M.dot(dang_exact)
-        dang_fd_soler = M.dot(dang_fd_soler)
-        assert np.allclose(dang_fd, dang_exact, atol=incr)
-        assert np.allclose(dang_fd_soler, dang_exact, atol=incr)
+        for ir, r in enumerate(zip(X.flatten(),Y.flatten(),Z.flatten())):
+            print(r)
+            vecspher = xc.projector.spher_grad.grlylm(n_l - 1, r) # shape: (3, n_l*n_l)
+            for il, vs in enumerate(vecspher.T):
+                dangs[il][ir] = vs
 
-
+        dangs = np.einsum('ij,jkl -> ikl', M, np.array(dangs))
+        dangs = dangs.reshape(len(dangs),*X.shape,3)[pick,:,:,:,inc_idx]
+        assert np.allclose(dangs, dang_fd, atol = incr)
+        
 @pytest.mark.skipif(not ase_found, reason='requires ase')
 @pytest.mark.force
 @pytest.mark.fast
@@ -148,15 +216,101 @@ def test_force_correction():
 
         species = benzene_traj.get_chemical_symbols()
         V, forces = benzene_nxc.get_V(rho, unitcell, grid, positions, species, calc_forces = True)[1]
-        V = V/Hartree
-        forces = forces/Hartree*Bohr
+        forces = forces
         Vp = benzene_nxc.get_V(rho, unitcell, grid, pp, species)[1]
         Vm = benzene_nxc.get_V(rho, unitcell, grid, pm, species)[1]
+        fig = xc.utils.visualize.plot_density_cut(Vp-Vm, rmax = [40]*3)
+        fig.savefig('./vpvm.pdf')
+        fig = xc.utils.visualize.plot_density_cut(Vp, rmax = [40]*3)
+        fig.savefig('./vp.pdf')
+
         dv = (unitcell[0,0]/grid[0])**3
+        fp = dv*np.sum(Vp*rho)
+        fm = dv*np.sum(Vm*rho)
 
-        fp = dv*np.sum(Vp*rho)/Hartree*Bohr
-        fm = dv*np.sum(Vm*rho)/Hartree*Bohr
-
-        print((fp-fm)/(2*incr))
+        forces_fd = (fp-fm)/(2*incr)
+        print(forces_fd)
         print(forces[incr_atom,incr_idx])
-    assert False
+        # assert np.allclose(forces_fd, forces[incr_atom, incr_idx])
+
+@pytest.mark.delbasis
+def test_del_basis():
+    benzene_traj = ase.io.read(os.path.join(test_dir,'benzene_test','benzene.xyz'),'0')
+    density_getter = xc.utils.SiestaDensityGetter(binary=True)
+    rho, unitcell, grid = density_getter.get_density(os.path.join(test_dir,
+                                'benzene_test','benzene.RHOXC'))
+    basis_set = {
+                'C': {'n' : 4, 'l' : 3, 'r_o': 1},
+                'H': {'n' : 4, 'l' : 2, 'r_o': 1.5}
+                }
+    positions = benzene_traj.get_positions()/Bohr
+    species = benzene_traj.get_chemical_symbols()
+
+    projector = xc.projector.DensityProjector(unitcell, grid, basis_set)
+    n = 0
+    l = 0
+    m = 0
+    for incr_atom in [0]:
+        incr = 0.001
+        incr_idx = 1
+        pp = np.array(positions)
+        pm = np.array(pp)
+        pp[incr_atom, incr_idx] += incr
+        pm[incr_atom, incr_idx] -= incr
+
+
+        basis_rep_p = projector.get_basis_rep_dict(rho, pp, species)['C'][0]['{},{},{}'.format(n,l,m)]
+        basis_rep_m = projector.get_basis_rep_dict(rho, pm, species)['C'][0]['{},{},{}'.format(n,l,m)]
+        print(basis_rep_p)
+        print(basis_rep_m)
+        print((basis_rep_p-basis_rep_m)/(2*incr))
+
+        box = projector.box_around(positions[0],1)
+        print(type(projector.W['C']))
+        dbasis = del_basis(projector, rho, box, n, l, m, 1, projector.W['C'])
+        print(dbasis[incr_idx])
+        assert False
+
+def del_basis(self, rho, box, n, l,m, r_o, W = None):
+    R, Theta, Phi = box['radial']
+    Xm, Ym, Zm = box['mesh']
+    X, Y, Z = box['real']
+    n_l = l +1
+    #Build angular part of basis functions
+    ang = self.angulars(l, m, Theta, Phi)
+
+    # Derivatives of spherical harmonic
+    M = xc.projector.M_make_complex(n_l)
+    dangs = []
+    for il in range(n_l**2):
+        dangs.append(np.zeros([len(X.flatten()),3]))
+
+    for ir, r in enumerate(zip(X.flatten(),Y.flatten(),Z.flatten())):
+        vecspher = xc.projector.spher_grad.grlylm(n_l - 1, r) # shape: (3, n_l*n_l)
+        for il, vs in enumerate(vecspher.T):
+            dangs[il][ir] = vs
+
+    dangs = np.einsum('ij,jkl -> ikl', M, np.array(dangs))
+
+    pick = sum([1 for l_ in range(l) for m_ in range(-l_,l_+1)])
+    pick += (l+m)
+    print(pick)
+    dangs = dangs.reshape(len(dangs),*X.shape,3)
+    dang = dangs[pick]
+
+    #Build radial part of b.f.
+    if not isinstance(W, np.ndarray):
+        W = self.get_W(r_o, n_rad) # Matrix to orthogonalize radial basis
+
+    drads = self.dradials(R, r_o, W)[n]
+    rads = self.radials(R, r_o, W)[n]
+    radsr = np.array(rads)
+    # radsr[R==0] = 0
+    radsr = radsr/R
+
+    rhat = [X/R, Y/R, Z/R]
+    rho = rho[box['mesh']]
+    print(np.linalg.norm(drads))
+    print(np.linalg.norm(dang[:,:,:,:]))
+    return [np.sum(rho*(ang * (drads - l*radsr) * rhat[ix]) + \
+                     (rads/(R**l)*dang[:,:,:,ix]))*self.V_cell for ix in range(3)]