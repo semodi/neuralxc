fde12e874b3d1f33b3acdd048c98f107146d2341
Sebastian Dick <sebastian.dick@stonybrook.edu>
2020-01-31 10:00:40 -0500

    fixed setup.py after deleting pyscf_ks script

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     # Comment out this line to prevent the files from being packaged with your software
     # Extend/modify the list to include/exclude other items as need be
     package_data={'neuralxc': ["data/*.dat"]},
-    scripts=['./bin/neuralxc', './bin/pyscf_ks'],
+    scripts=['./bin/neuralxc'],
     ext_modules=[spher_grad],
     # Additional entries you may want simply uncomment the lines you want and fill in the data
     # author_email='me@place.org',      # Author email
