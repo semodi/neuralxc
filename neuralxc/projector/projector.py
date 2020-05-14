fixed rmax calculation for non-orthogonal ucells

neuralxc/projector/projector.py
@@ -585,7 +585,7 @@ class DefaultProjector(BaseProjector):
         pos = pos.flatten()
 
         #Create box with max. distance = radius
-        rmax = np.ceil(radius / self.a).astype(int).tolist()
+        rmax = (np.ceil(radius / self.a).astype(int)+2).tolist()
         Xm, Ym, Zm = mesh_3d(self.U, self.a, scaled=False, rmax=rmax, indexing='ij')
         X, Y, Z = mesh_3d(self.U, self.a, scaled=True, rmax=rmax, indexing='ij')
 